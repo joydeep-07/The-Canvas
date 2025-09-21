7f82afe70fa1b4a8cd49f56c745cfa67750dd5a7
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-09-21 19:06:55 +0530

    Landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const LandingPage = () => {
   return (
-    <div className='min-h-screen flex justify-center items-center'>
-        <h1 className='text-3xl'>This is Landing Page</h1>
-    </div>
-  )
-}
+    <div className="min-h-screen flex flex-col justify-center items-center px-4">
+      {/* Intro text */}
+      <p className="text-gray-700 mb-2 uppercase">
+        Hi, myself Paul and I am a freelancer !
+      </p>
 
-export default LandingPage
+      {/* Main heading */}
+      <div>
+        <h1 className="text-5xl sm:text-9xl p-5 font-bold leading-tight">
+          <span className="block text-black text-center">Webdesigner</span>
+          <span className="block text-transparent stroke-text">
+            & Photographer
+          </span>
+        </h1>
+
+        {/* Location */}
+        <p className="text-gray-600 justify-between flex px-5 mt-2">
+          <span>Based in Kolkata, India.</span>
+          <span>
+            <ul className="flex gap-5">
+              <li>Mongo Db</li>
+              <li>Express Js</li>
+              <li>React Js</li>
+              <li>Node Js</li>
+            </ul>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
