53218d09d175f06a319f639e0fe1d56a41583b53
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-09-21 19:17:03 +0530

    Updated

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,27 +1,24 @@
 import React from "react";
+import "../style/Landing.css";
 
 const LandingPage = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center px-4">
+    <div className="landing-container">
       {/* Intro text */}
-      <p className="text-gray-700 mb-2 uppercase">
-        Hi, myself Paul and I am a freelancer !
-      </p>
+      <p className="intro-text">Hi, myself Paul and I am a freelancer !</p>
 
       {/* Main heading */}
       <div>
-        <h1 className="text-5xl sm:text-9xl p-5 font-bold leading-tight">
-          <span className="block text-black text-center">Webdesigner</span>
-          <span className="block text-transparent stroke-text">
-            & Photographer
-          </span>
+        <h1 className="main-heading">
+          <span className="heading-black">Web Designer</span>
+          <span className="heading-outline">& Web Developer</span>
         </h1>
 
         {/* Location */}
-        <p className="text-gray-600 justify-between flex px-5 mt-2">
+        <p className="location">
           <span>Based in Kolkata, India.</span>
           <span>
-            <ul className="flex gap-5">
+            <ul className="tech-stack">
               <li>Mongo Db</li>
               <li>Express Js</li>
               <li>React Js</li>
