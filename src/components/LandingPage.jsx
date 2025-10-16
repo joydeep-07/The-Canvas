473d9aec9f6b627fa8e0160441521b45eb5d06a0
Joydeep Paul <joydeeprnp8821@gmail.com>
2025-10-16 16:11:41 +0530

    Updated

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -49,21 +49,11 @@ const LandingPage = () => {
               The Art of Code a Digital <br /> Symphony of Ideas & Innovation
             </h1>
             <p className="max-w-lg pt-5 text-gray-700 leading-relaxed">
-              We strive towards a simple but ambitious mission: to reform the
-              education system by offering world-class learning rooted in
-              experience and real-life application at a fraction of the cost of
-              traditional academic institutions.
+              Driven MERN Stack Developer turning ideas into functional,
+              beautiful web apps. Constantly exploring new technologies to
+              deliver meaningful user experiences and impactful digital
+              solutions that inspire and connect.
             </p>
-
-            {/* Buttons */}
-            <div className="pt-6 flex gap-4">
-              <button className="px-6 py-2 rounded-full bg-blue-500 text-white font-medium shadow-md hover:bg-blue-600 transition-all">
-                Join Now
-              </button>
-              <button className="px-6 py-2 rounded-full border-2 border-gray-400 hover:border-blue-500 hover:text-blue-500 font-medium transition-all">
-                Explore
-              </button>
-            </div>
           </div>
         </div>
       </div>
